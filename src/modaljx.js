b8611b55db5078a2d2d09cdb18cb1c7a8bdf91a8
Muhammet Çakır <m-cakir@users.noreply.github.com>
2017-01-18 17:05:19 +0300

    Update modaljx.js

--- src/modaljx.js
+++ src/modaljx.js
@@ -29,21 +29,21 @@
             });
         },
 		
-		open : function(url){
+        open : function(url){
 			
-			if(!Boolean(url)) {
+            if(!Boolean(url)) {
 				
-				this.__log("URL cannot be empty!");
+                this.__log("URL cannot be empty!");
 				
-				return;
+                return;
 				
-			}
+            }
 
-			if(url.charAt(0) === '#') url = url.substr(1);
+            if(url.charAt(0) === '#') url = url.substr(1);
 
-			var options = this.options;
+            var options = this.options;
 
-			var modalOptions = options.staticBackdrop ? { backdrop: 'static', keyboard: false } : {};
+            var modalOptions = options.staticBackdrop ? { backdrop: 'static', keyboard: false } : {};
 
             this._modal = $(this._getHtml()).appendTo('body').modal(modalOptions);
 
@@ -52,74 +52,76 @@
             this._modal.on('shown.bs.modal', function () {
 
                 if (typeof( $('body').data('fv_open_modals') ) == 'undefined') {
-					$('body').data('fv_open_modals', 0);
-				}
+			
+                    $('body').data('fv_open_modals', 0);
+			
+                }
 
-				if ($(this).hasClass('fv-modal-stack')) return;
+                if ($(this).hasClass('fv-modal-stack')) return;
 
-				var modalCount = $('body').data('fv_open_modals');
+                var modalCount = $('body').data('fv_open_modals');
 
-				$('body').data('fv_open_modals', modalCount + 1);
+                $('body').data('fv_open_modals', modalCount + 1);
 
-				$(this).addClass('fv-modal-stack').css('z-index', 1040 + (10 * modalCount));
+                $(this).addClass('fv-modal-stack').css('z-index', 1040 + (10 * modalCount));
 
-				$('.modal-backdrop').not('.fv-modal-stack').css('z-index', 1039 + (10 * modalCount)).addClass('fv-modal-stack');
+                $('.modal-backdrop').not('.fv-modal-stack').css('z-index', 1039 + (10 * modalCount)).addClass('fv-modal-stack');
 					
-				if(typeof window[options.onShown] === 'function'){
+                if(typeof window[options.onShown] === 'function'){
 					
-					window[options.onShown].apply(this);
+                    window[options.onShown].apply(this);
 					
-				}
+                }
 
             }).on('hidden.bs.modal', function(){
 
                 if(this.xhr) this.xhr.abort();
 
-				$('body').data('fv_open_modals', $('body').data('fv_open_modals' ) - 1);
+                $('body').data('fv_open_modals', $('body').data('fv_open_modals' ) - 1);
 
-				$(this).removeClass('fv-modal-stack').remove();
+                $(this).removeClass('fv-modal-stack').remove();
 					
-				if(typeof window[options.onClose] === 'function') {
+                if(typeof window[options.onClose] === 'function') {
 					
-					window[options.onClose].apply(this);
+                    window[options.onClose].apply(this);
 					
-				}
+                }
 				
             });
 			
-			this.__ajax(url);
+            this.__ajax(url);
 			
-		},
+        },
 
-		hide : function () {
+        hide : function () {
 
-			if(this._modal) this._modal.modal('hide');
+            if(this._modal) this._modal.modal('hide');
 
-		},
+        },
 		
-		__ajax : function(url){
+        __ajax : function(url){
 			
-			var options = this.options;
+            var options = this.options;
 			
-			var modal = this._modal;
+            var modal = this._modal;
 			
-			var content = this._content;
+            var content = this._content;
 			
             this.xhr = $.ajax({
                 
-				type    	: options.ajax.type,
+                type    	: options.ajax.type,
                 
-				url     	: url,
+                url     	: url,
                 
-				data    	: this._getData(),
+                data    	: this._getData(),
 
-				beforeSend 	: options.ajax.beforeSend
+                beforeSend 	: options.ajax.beforeSend
 				
             }).done(function(response, status, xhr){
 				
                 if(status !== 'success') return;
 				
-				modal.addClass(options.modalClass);
+                modal.addClass(options.modalClass);
 
                 content.html(response);
 
@@ -127,34 +129,34 @@
 					
                     window[options.onSuccess].apply(this, [].concat(arguments));
 					
-				}
+                }
 				
             }).fail(function() {
 				
-				if(options.canHideOnFail) modal.modal('hide');
+                if(options.canHideOnFail) modal.modal('hide');
 
                 if(typeof window[options.onFail] === 'function') {
                 
-					window[options.onFail].apply(this, [].concat(arguments));
+                    window[options.onFail].apply(this, [].concat(arguments));
 				
-				}
+                }
 				
-			});
+            });
 			
-		},
+        },
 		
-		_getData : function(){
+        _getData : function(){
 			
-			var data = {};
+            var data = {};
 			
-			if(!this.options.cacheable){
+            if(!this.options.cacheable){
 				
-				data.r = this.__random(100, 999999999);
+                data.r = this.__random(100, 999999999);
 				
-			}
+            }
 			
-			return data;
-		},
+            return data;
+        },
 
         _getHtml : function(){
 
@@ -178,13 +180,13 @@
             return Math.floor( Math.random() * (n - m + 1) ) + m;
         },
 		
-		__log : function(txt){
+        __log : function(txt){
 			
-			if(typeof console === 'undefined') return;
+            if(typeof console === 'undefined') return;
 			
-			console.log("modaljx - msg: " + txt);
+            console.log("modaljx - msg: " + txt);
 			
-		}
+        }
 
     };
 
@@ -205,31 +207,31 @@
 
     $.fn.modaljx.defaults = {
 		
-		cacheable	: false,
+        cacheable	: false,
 
-		canHideOnFail : false,
+        canHideOnFail : false,
 
-		staticBackdrop : false,
+        staticBackdrop : false,
         
-		modalClass	: '',	
+        modalClass	: '',	
 		
         spinner     : '<div class="progress progress-striped active" style="margin-top: 10%;"><div class="progress-bar progress-bar-info" style="width: 100%"></div></div>',
 		
-		ajax 		: {
+        ajax 		: {
 				
-			type : 'GET', // or POST
+            type : 'GET', // or POST
 
-			beforeSend : function () {}
+            beforeSend : function () {}
 				
-		},
+        },
 				
         onFail     	: function(){},
 		
-		onSuccess	: function(){},
+        onSuccess	: function(){},
 		
         onShown   	: function(){},
 		
-		onClose		: function(){}
+        onClose		: function(){}
     };
 
 }(jQuery));
